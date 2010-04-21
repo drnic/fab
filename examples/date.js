7c213df30889ec476d69db9d3faaaa1536972469
Dr Nic Williams <drnicwilliams@gmail.com>
2010-04-21 11:53:37 +1000

    Showing URLs for the example

--- examples/date.js
+++ examples/date.js
@@ -1,18 +1,22 @@
 fab = require( "../" )
 
+// URLs below assume module hosted below /date path.
 module.exports = fab
 
   ( fab.contentLength )
   ( fab.stringify )
   
+  // URL: /date/date
   ( /\/date/ )
     ( fab.tmpl )
       ( "The date is <%= this.toDateString() %>." )
     ()
 
+  // URL: /date/time
   ( /\/time/ )
     ( fab.tmpl )
       ( "The time is <%= this.toTimeString() %>." )
     ()
   
+  // URL: /date
   ( new Date );
